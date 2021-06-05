ab9453c05d3e9034ec295589d98c9d924c7db134
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-06-05 23:34:43 +0300

    Exporting load_settings

--- src/EasyML.jl
+++ src/EasyML.jl
@@ -40,7 +40,7 @@ export QML, Flux, FluxExtra, CUDA, NNlib, ColorTypes
 export model_data, Image_classification_class, Image_segmentation_class, training, settings, training_data, training_plot_data,
     training_results_data, training_options, validation_data, validation_results_data,
     application_data, application_options
-export design_network, modify_classes, modify_output, modify, save_model, load_model, get_urls_training, prepare_training_data, train,
+export load_settings,design_network, modify_classes, modify_output, modify, save_model, load_model, get_urls_training, prepare_training_data, train,
     get_urls_validation, validate, get_urls_application, apply, forward, apply_border_data
 export Join, Split, Addition, Activation, Identity
 
