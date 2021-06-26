bfe1ff2cebfbbc777fcff9f7b61951be83c0bfda
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-06-27 00:29:56 +0300

    Exporting hardware_resources and graphics

--- src/EasyML.jl
+++ src/EasyML.jl
@@ -40,7 +40,7 @@ export QML, Flux, FluxExtra, CUDA, NNlib, ColorTypes
 
 export model_data, ImageClassificationClass, ImageRegressionClass, ImageSegmentationClass, training, settings, training_data, 
     training_plot_data, training_results_data, training_options, validation_data, validation_results_data,
-    application_data, application_options
+    application_data, application_options, hardware_resources, graphics
 export load_settings, design_network, modify_classes, modify_output, modify, save_model, load_model, 
     get_urls_training, prepare_training_data, remove_training_data, remove_training_results, train, get_urls_validation, 
     validate, remove_validation_data, get_urls_application, apply, remove_application_data, forward, apply_border_data
