a24c050c95cf45b0ba3ea987a5ea3eafb60903dc
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-07-04 19:40:18 +0300

    Removed from export

--- src/EasyMLTraining.jl
+++ src/EasyMLTraining.jl
@@ -29,7 +29,7 @@ include("exported_functions.jl")
 
 export QML, Flux, FluxExtra, CUDA, NNlib, ColorTypes
 
-export model_data, options, global_options, training_data, training_results_data, training_options
+export model_data, global_options, training_options, training_results_data
 export load_options, modify, set_savepath, save_model, load_model, set_problem_type, set_weights, set_training_data, set_testing_data, train,
     remove_training_data, remove_testing_data, remove_training_results
 export Join, Split, Addition, Activation, Identity
