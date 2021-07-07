dfb253c59435389f71643215f761ef93b9f26254
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-07-07 23:43:18 +0300

    Removed unused functions

--- src/Common/all.jl
+++ src/Common/all.jl
@@ -146,7 +146,7 @@ end
 get_options(fields,inds...) = get_options_main(options,fields,inds...)
 
 # Allows to write to options from GUI
-function set_options_main(options::Options,fields::QML.QListAllocated,args...)
+function set_options_main(options::Options,fields,args...)
     data = options
     fields = fix_QML_types(fields)
     args = fix_QML_types(args)
@@ -258,51 +258,9 @@ function dict_to_struct!(obj,dict::Dict)
     return nothing
 end
 
-
-#---Clean up----------------------------------------------------
-
-function empty_field!(str,field::Symbol)
-    val = getfield(str,field)
-    type = typeof(val)
-    new_val = type(undef,zeros(Int64,length(size(val)))...)
-    setfield!(str, field, new_val)
-    return nothing
-end
-
-function reset_data_field_main(all_data::AllData,fields)
-    fields::Vector{String} = fix_QML_types(fields)
-    data = all_data
-    for i = 1:length(fields)
-        field = Symbol(fields[i])
-        data = getproperty(data,field)
-    end
-    empty!(data)
-    return nothing
-end
-reset_data_field(fields) = reset_data_field_main(all_data,fields)
-
-function resetproperty!(datatype,field)
-    var = getproperty(datatype,field)
-    if var isa Array
-        var = similar(var,0)
-    elseif var isa Number
-        var = zero(typeof(var))
-    elseif var isa String
-        var = ""
-    end
-    setproperty!(datatype,field,var)
-    return nothing
-end
-
 #---Other-------------------------------------------------------------
 
 problem_type() = model_data.problem_type
-
-add_dim(x::Array{T, N}) where {T,N} = reshape(x, Val(N+1))
-
-function make_tuple(array::AbstractArray)
-    return (array...,)
-end
 
 function make_dir(target_dir::AbstractString)
     dirs = split(target_dir,('/','\\'))
