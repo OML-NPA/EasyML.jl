15b1e9281380a574b13b7bae34984abffff2b888
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-07-07 22:22:28 +0300

    Improved tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,9 +6,30 @@ EasyMLDesign.unit_test.state = true
 set_savepath("models/test.model")
 set_problem_type(:Classification)
 
-load_model("models/test.model")
-
+# Empty model
 design_model()
+
+# Flatten error model
+load_model("models/flatten_error_test.model")
+design_model()
+
+# No output error model
+load_model("models/no_output_error_test.model")
+design_model()
+
+# All layers test model
+load_model("models/all_test.model")
+design_model()
+
+# Losses
+load_model("models/minimal_test.model")
+losses = ["MAE","MSE","MSLE","Huber","Crossentropy","Logit crossentropy","Binary crossentropy",
+    "Logit binary crossentropy","Kullback-Leiber divergence","Poisson","Hinge","Squared hinge",
+    "Dice coefficient","Tversky"]
+for i = 1:length(losses)
+    model_data.layers_info[end].loss = (losses[i],i+1)
+    design_model()
+end
 
 save_model("models/test.model")
 load_options()
