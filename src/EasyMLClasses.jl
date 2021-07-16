e3b13f3f2752681948bc78142ab733739b93c337
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-07-16 20:05:48 +0300

    Fixed exports/imports

--- src/EasyMLClasses.jl
+++ src/EasyMLClasses.jl
@@ -2,22 +2,20 @@
 module EasyMLClasses
 
 # Import packages
-using
-# Machine learning
-Flux, FluxExtra
-
-using EasyMLCore, EasyMLCore.Classes
+using 
+# EasyML ecosystem
+EasyMLCore, EasyMLCore.Classes
 
 # Include functions
 include("common/design_classes.jl")
 include("main.jl")
 include("exported_functions.jl")
 
-export QML, Flux, FluxExtra
+export QML, Flux, Losses, FluxExtra, Normalizations, NNlib
 
 export model_data, Classification, Regression, Segmentation, Image, 
     ImageClassificationClass, ImageRegressionClass, ImageSegmentationClass
-export save_model, load_model
+export set_savepath, set_problem_type, set_input_type, save_options, load_options, save_model, load_model
 export make_classes
 
 end
