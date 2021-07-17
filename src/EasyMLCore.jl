2abfa86433bf30a111333ec145fd5383cb06f239
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-07-17 12:03:51 +0300

    Exporting all_data, AllData

--- src/EasyMLCore.jl
+++ src/EasyMLCore.jl
@@ -43,19 +43,17 @@ export AbstractInputType, Image
 export AbstractInputProperty, Grayscale
 # Struct to Dict interconversion
 export struct_to_dict!, dict_to_struct!, to_struct!
-# Model saving/loading
+# Model
 export ModelData, AbstractModel, set_savepath, save_model, load_model
-# Options saving/loading
-export options, Options, save_options, load_options
+# Options
+export modify, global_options, options, Options, save_options, load_options
 # GUI data handling
 export fix_QML_types, get_data, get_options, set_data, set_options, get_file, get_folder
 # Handling channels
 export check_progress, get_progress, empty_channel, put_channel
-# Options
-export modify, global_options
 # Other
-export set_problem_type, set_input_type, problem_type, input_type, model_data, check_task, 
-    unit_test, add_templates
+export all_data, AllData, set_problem_type, set_input_type, problem_type, input_type, 
+    model_data, check_task, unit_test, add_templates
 
 # QML functions
 export QML, @qmlfunction, QByteArray, loadqml, exec
