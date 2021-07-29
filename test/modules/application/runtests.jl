5b983e1a7ec7a38e691b3549e0d10a96007b0e52
a-ill <a_ill@outlook.com>
2021-07-29 17:07:18 +0300

    Fixed application tests

--- test/modules/application/runtests.jl
+++ test/modules/application/runtests.jl
@@ -11,23 +11,23 @@ global_options.HardwareResources.num_slices = 20
 modify(application_options)
 modify_output()
 
-training_options.Testing.data_preparation_mode = :auto
-training_options.Testing.test_data_fraction = 0.2
-load_model(joinpath(models_dir(),"classification.model"))
-url_input = joinpath(examples_dir(),"classification/test")
+load_model(joinpath(models_dir,"classification.model"))
+url_input = joinpath(examples_dir,"classification/test")
 get_urls_application(url_input)
 apply()
 
-load_model(joinpath(models_dir(),"regression.model"))
-url_input = joinpath(examples_dir(),"regression/test")
+load_model(joinpath(models_dir,"regression.model"))
+url_input = joinpath(examples_dir,"regression/test")
 get_urls_application(url_input)
 apply()
 
 training_options.Testing.test_data_fraction = 0.5
-load_model(joinpath(models_dir(),"segmentation.model"))
-url_input = joinpath(examples_dir(),"segmentation/images")
+load_model(joinpath(models_dir,"segmentation.model"))
+url_input = joinpath(examples_dir,"segmentation/images")
 get_urls_application(url_input)
 apply()
+
+rm("Output data",recursive=true)
 
 
 #---Other QML-------------------------------------------------------
