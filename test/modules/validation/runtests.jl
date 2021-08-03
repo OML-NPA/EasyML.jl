404d907a56625fbad66183a9ffd15d7e29be6265
a-ill <a_ill@outlook.com>
2021-08-03 13:53:02 +0300

    Changed validation images

--- test/modules/validation/runtests.jl
+++ test/modules/validation/runtests.jl
@@ -4,7 +4,7 @@ import EasyML.Validation
 
 cd(@__DIR__)
 
-global_options.HardwareResources.num_slices = 2
+global_options.HardwareResources.num_slices = 1
 
 
 #---Main functionality---------------------------------------------------------
@@ -15,74 +15,128 @@ global_options.HardwareResources.num_slices = 2
 end
 rm("options.bson")
 
-for i = 1:2
-    if i==1
-        validation_options.Accuracy.weight_accuracy = true
-        global_options.HardwareResources.allow_GPU = true
-    else
-        validation_options.Accuracy.weight_accuracy = false
-        global_options.HardwareResources.allow_GPU = false
-    end
+validation_options.Accuracy.weight_accuracy = true
+global_options.HardwareResources.allow_GPU = true
 
-    @testset "Classfication" begin
-        load_model(joinpath(models_dir,"classification.model"))
-        @test begin 
-            change_classes()
-            true
-        end
-        @test begin 
-            url_data = "examples/with labels/classification/test"
-            get_urls_validation(url_data)
-            true
-        end
-        @test begin 
-            results = validate()
-            remove_validation_data()
-            remove_validation_results()
-            true
-        end
+@testset "Classfication" begin
+    load_model(joinpath(models_dir,"classification.model"))
+    @test begin 
+        change_classes()
+        true
     end
-
-    @testset "Regression" begin
-        load_model(joinpath(models_dir,"regression.model"))
-        @test begin 
-            change_classes()
-            true
-        end
-        @test begin 
-            url_input = "examples/with labels/regression/test"
-            url_labels = "examples/with labels/regression/test.csv"
-            get_urls_validation(url_input,url_labels)
-            true
-        end
-        @test begin
-            results = validate()
-            remove_validation_data()
-            remove_validation_results()
-            true
-        end
+    @test begin 
+        url_data = "examples/with labels/classification/test"
+        get_urls_validation(url_data)
+        true
     end
-
-    @testset "Segmentation" begin
-        load_model(joinpath(models_dir,"segmentation.model"))
-        @test begin 
-            change_classes()
-            true
-        end
-        @test begin 
-            url_input = "examples/with labels/segmentation/images"
-            url_labels = "examples/with labels/segmentation/labels"
-            get_urls_validation(url_input,url_labels)
-            true
-        end
-        @test begin
-            results = validate()
-            remove_validation_data()
-            remove_validation_results()
-            true
-        end
+    @test begin 
+        results = validate()
+        remove_validation_data()
+        remove_validation_results()
+        true
     end
 end
+
+@testset "Regression" begin
+    load_model(joinpath(models_dir,"regression.model"))
+    @test begin 
+        change_classes()
+        true
+    end
+    @test begin 
+        url_input = "examples/with labels/regression/test"
+        url_labels = "examples/with labels/regression/test.csv"
+        get_urls_validation(url_input,url_labels)
+        true
+    end
+    @test begin
+        results = validate()
+        remove_validation_data()
+        remove_validation_results()
+        true
+    end
+end
+
+@testset "Segmentation" begin
+    load_model(joinpath(models_dir,"segmentation.model"))
+    @test begin 
+        change_classes()
+        true
+    end
+    @test begin 
+        url_input = "examples/with labels/segmentation/images"
+        url_labels = "examples/with labels/segmentation/labels"
+        get_urls_validation(url_input,url_labels)
+        true
+    end
+    @test begin
+        results = validate()
+        remove_validation_data()
+        remove_validation_results()
+        true
+    end
+end
+
+validation_options.Accuracy.weight_accuracy = false
+global_options.HardwareResources.allow_GPU = false
+
+@testset "Classfication" begin
+    load_model(joinpath(models_dir,"classification.model"))
+    @test begin 
+        change_classes()
+        true
+    end
+    @test begin 
+        url_data = "examples/without labels/classification/test"
+        get_urls_validation(url_data)
+        true
+    end
+    @test begin 
+        results = validate()
+        remove_validation_data()
+        remove_validation_results()
+        true
+    end
+end
+
+@testset "Regression" begin
+    load_model(joinpath(models_dir,"regression.model"))
+    @test begin 
+        change_classes()
+        true
+    end
+    @test begin 
+        url_input = "examples/without labels/regression/test"
+        get_urls_validation(url_input)
+        true
+    end
+    @test begin
+        results = validate()
+        remove_validation_data()
+        remove_validation_results()
+        true
+    end
+end
+
+@testset "Segmentation" begin
+    load_model(joinpath(models_dir,"segmentation.model"))
+    @test begin 
+        change_classes()
+        true
+    end
+    @test begin 
+        url_input = "examples/without labels/segmentation/images"
+        get_urls_validation(url_input)
+        true
+    end
+    @test begin
+        results = validate()
+        remove_validation_data()
+        remove_validation_results()
+        true
+    end
+end
+
 
 
 #---Other QML---------------------------------------------------------
