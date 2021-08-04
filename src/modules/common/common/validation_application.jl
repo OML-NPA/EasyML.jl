45b5713055d4872dc7cc2d8ab81c79cccbbbcbd5
a-ill <a_ill@outlook.com>
2021-08-04 15:17:00 +0300

    Fixed pad

--- src/modules/common/common/validation_application.jl
+++ src/modules/common/common/validation_application.jl
@@ -194,12 +194,17 @@ end
 
 function pad(array::A,padding::NTuple{2,Int64},
         fun::Union{typeof(zeros),typeof(ones)}) where {T<:AbstractFloat,A<:AbstractArray{T, 4}}
+    #=
+    array = ones(Float32,5,5,1,1)
+    padding = (2,2)
+    fun = ones
+    A = typeof(array)=#
     div_result = padding./2
     leftpad = Int64.(floor.(div_result))
     rightpad = Int64.(ceil.(div_result))
     if padding[1]!=0
         accum = Vector{A}(undef,0)
-        for i in 1:size(array,3)
+        for i=1:size(array,3)
             temp_array = array[:,:,i,:,:]
             s_ar2 = size(temp_array,2)
             s1 = (leftpad[1],s_ar2,1,1)
@@ -207,11 +212,12 @@ function pad(array::A,padding::NTuple{2,Int64},
             output_array = vcat(fun(T,s1),temp_array,fun(T,s2))
             push!(accum,output_array)
         end
-        array = cat(accum,dims=Val(3))
+        array = cat(accum...,dims=Val(3))
     end
     if padding[2]!=0
         accum = Vector{A}(undef,0)
-        for i in 1:size(array,3)   
+        @info Vector{A}
+        for i=1:size(array,3)   
             temp_array = array[:,:,i,:,:]
             s_ar1 = size(temp_array,1)
             s1 = (s_ar1,leftpad[2],1,1)
@@ -219,7 +225,7 @@ function pad(array::A,padding::NTuple{2,Int64},
             output_array = hcat(fun(T,s1),temp_array,fun(T,s2))
             push!(accum,output_array)
         end
-        array = cat(accum,dims=Val(3))
+        array = cat(accum...,dims=Val(3))
     end
     return array
 end
